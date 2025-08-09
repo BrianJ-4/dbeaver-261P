bb2e650773111e04d7ca613cd4098c088954b700
serge-rider <serge@dbeaver.com>
2025-08-09 08:01:45 +0200

    dbeaver/pro#2567 Database doc links fix

--- plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/dialogs/connection/ConnectionPageAbstract.java
+++ plugins/org.jkiss.dbeaver.ui.editors.connection/src/org/jkiss/dbeaver/ui/dialogs/connection/ConnectionPageAbstract.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -266,7 +266,7 @@ public abstract class ConnectionPageAbstract extends DialogPage implements IData
     private void formDatabaseDocumentationInfoLabel(Composite panel) {
         databaseDocumentationInfoLabel = UIUtils.createInfoLabel(
             panel,
-            UIConnectionMessages.dialog_connection_database_documentation,
+            site.getDriver().getFullName() + " ",
             () -> {
                 String databaseDocumentationSuffixURL = site.getDriver().getDatabaseDocumentationSuffixURL();
                 ShellUtils.launchProgram(HelpUtils.getHelpExternalReference(databaseDocumentationSuffixURL));
